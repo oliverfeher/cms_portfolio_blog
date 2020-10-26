87956b9152e0db6dd683a7ba2f668023d0581ac7
Oliver Feher <oliverfeher@Olivers-MacBook-Pro.local>
2020-10-25 22:48:19 -0400

    404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,49 @@
-import React from 'react'
-import { Heading, Box } from '@chakra-ui/core';
+import React, { useEffect } from 'react'
 import Layout from '../components/layout';
+import { navigate } from '@reach/router';
 
-export default () => (
-    <Layout>
-        <Box>
-            <Heading as="h1" size="xl">Oops, that's a 404</Heading>
-        </Box>
-    </Layout>
-)
+import { graphql } from 'gatsby';
+
+export default ({ data }) => {
+    
+    useEffect(() => {
+       setTimeout(() => navigate('/'), 3000)
+    }, [])
+
+    const renderHTML = (string) => {
+        return {__html: string};
+      }
+
+    return (
+        <Layout>
+            <main>
+                <div className="portrait-container">
+                    <img src={data.wpPage.pageBlocks.pageblocks[1].pictures[0].sourceUrl} alt="portrait headshot" />
+                </div>
+
+                <div className="intro-wysiwyg" dangerouslySetInnerHTML={renderHTML(data.wpPage.pageBlocks.pageblocks[0].text)} />
+            </main>
+        </Layout>
+    )
+}
+
+export const query = graphql`
+  query fourOfourQuery {
+    wpPage(slug: {eq: "404-data"}) {
+      pageBlocks {
+        pageblocks {
+          ... on WpPage_Pageblocks_Pageblocks_TextEditor {
+            text
+            fieldGroupName
+          }
+          ... on WpPage_Pageblocks_Pageblocks_Pictures {
+            fieldGroupName
+            pictures {
+              sourceUrl
+            }
+          }
+        }
+      }
+    }
+  }
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,91 @@
 // DEPENDENCY IMPORTS
 import React, { useState } from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 
+// ASSET/COMPONENT IMPORTS
+import social1 from '../assets/svg/github-svg.svg';
+import social2 from '../assets/svg/linkedin-svg.svg';
+import social3 from '../assets/svg/twitter-svg.svg';
 import Layout from '../components/layout';
-import renderHelper from '../utils/renderHelper';
 
 const IndexPage = ({ data }) => {
 
-  const [ portraits, setPortraits ] = useState(data.wpPage.pageBlocks.pageblocks[0].pictures)
-  const transformArray = (array) => {
-    for (let i = 0; i < array.length; i++) {
-        if (array[i].fieldGroupName) {
-            array[i].fieldGroupName = array[i].fieldGroupName.replace('post_Pageblocks_Blocks_', '');
-        }
-      }
-      return array;
+  const menus = [
+    {
+      label: 'About',
+      url: '/about'
+    },
+    {
+      label: 'Blog',
+      url: '/blog'
+    },
+    {
+      label: 'Contact',
+      url: '/contact'
     }
-    
-  //   const indexPageData = useStaticQuery(graphql`
-    
-  // `)
-  
-  // const normalizedArray = transformArray(indexPageData.wpPost.pageBlocks.blocks);
-  // {normalizedArray.map((block, idx) => renderHelper(block, idx))}
+  ]
 
-  console.log(data.wpPage.pageBlocks);
-  console.log(portraits);
+  const social = [
+    {
+      image: social1,
+      url: 'https://github.com/oliverfeher',
+    },
+    {
+      image: social2,
+      url: 'https://www.linkedin.com/in/oliver-feher-10093912a/',
+    },
+    {
+      image: social3,
+      url: 'https://twitter.com/Oliver92F',
+    },
+  ]
+
+  const renderHTML = (string) => {
+    return {__html: string};
+  }
+
+  const [ portraits ] = useState(data.wpPage.pageBlocks.pageblocks[0].pictures);
+  const [ currentPortrait, setCurrentPortraits ] = useState(portraits[2].sourceUrl)
+
+  const menuHover = (e) => {
+    switch(e.target.innerText) {
+      case "About":
+        setCurrentPortraits(portraits[1].sourceUrl);
+        break;
+      case "Blog":
+        setCurrentPortraits(portraits[0].sourceUrl);
+        break;
+      case "Contact":
+        setCurrentPortraits(portraits[3].sourceUrl);
+        break;
+      default:
+        setCurrentPortraits(portraits[2].sourceUrl);
+
+    }
+  }
+
 
   return (
     <Layout>
       <main>
         <div className="portrait-container">
-          <img src={portraits[2].sourceUrl} alt="portrait headshot" />
+          <img src={currentPortrait} alt="portrait headshot" />
+        </div>
+
+        <div className="intro-wysiwyg" dangerouslySetInnerHTML={renderHTML(data.wpPage.pageBlocks.pageblocks[1].text)} />
+
+        <div className="menus-container">
+          {menus.map((menu, idx) => {
+            return (
+              <Link className="menu-item" to={menu.url} key={idx} onMouseLeave={()=> setCurrentPortraits(portraits[2].sourceUrl)} onMouseEnter={menuHover}>{menu.label}</Link>
+            )
+          })}
+        </div>
+
+        <div className="social-icons">
+          {social.map((icon, idx) => {
+            return <a key={idx} rel="noreferrer" target="_blank" href={icon.url}><img src={icon.image} alt="social icon" /></a>
+          })}
         </div>
       </main>
     </Layout>
@@ -41,7 +95,7 @@ const IndexPage = ({ data }) => {
 export default IndexPage;
 
 export const query = graphql`
-  query MyQuery {
+  query IndexQuery {
     wpPage(slug: {eq: "index-data"}) {
       pageBlocks {
         pageblocks {
